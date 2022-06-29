83beccb14c4f07675a81b1ebcf46237f6b1b9a38
Jakub <jakub.micorek@icg.tugraz.at>
2022-06-29 13:30:13 +0200

    save uv and rgb images

--- demo.py
+++ demo.py
@@ -13,6 +13,7 @@ from raft import RAFT
 from utils import flow_viz
 from utils.utils import InputPadder
 
+from pathlib import Path
 
 
 DEVICE = 'cuda'
@@ -35,8 +36,9 @@ def viz(img, flo):
     # plt.imshow(img_flo / 255.0)
     # plt.show()
 
-    cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
-    cv2.waitKey()
+    cv2.imshow('image', img_flo[:, :, [2, 1, 0]] / 255.0)
+    cv2.waitKey(1)
+    return flo[:, :, [2, 1, 0]]
 
 
 def demo(args):
@@ -60,7 +62,34 @@ def demo(args):
             image1, image2 = padder.pad(image1, image2)
 
             flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
-            viz(image1, flow_up)
+
+            rgb_flow = viz(image1, flow_up)
+
+            # save flow as rgb image
+            # FIXME: will divide by max magnitude in frame
+            flow_rgb_path = os.path.join(os.path.dirname(imfile1), "flow_rgb")
+            Path(flow_rgb_path).mkdir(parents=True, exist_ok=True)
+            image_file_name_1_flow_rgb = f'{os.path.basename(imfile1).split(".")[0]}.jpg'
+            cv2.imwrite(os.path.join(flow_rgb_path, image_file_name_1_flow_rgb), rgb_flow)
+
+            flow_up = flow_up.cpu().numpy()
+            # save flow as uv vectors. 32bit floats
+            flow_uv_path = os.path.join(os.path.dirname(imfile1), "flow_uv")
+            image_file_name_1_flow_uv = f'{os.path.basename(imfile1).split(".")[0]}.npy'
+            Path(flow_uv_path).mkdir(parents=True, exist_ok=True)
+
+            with open(os.path.join(flow_uv_path, image_file_name_1_flow_uv), 'wb') as f:
+                np.save(f, flow_up)
+
+            # # save flow as sparse uv vectors. ignore pixel shifts of smaller sparse_threshold
+            # sparse_threshold = 0.9
+            # flow_uv_sparse_path = os.path.join(os.path.dirname(imfile1), "flow_uv_sparse")
+            # image_file_name_1_flow_uv = f'{os.path.basename(imfile1).split(".")[0]}.npz'
+            # Path(flow_uv_sparse_path).mkdir(parents=True, exist_ok=True)
+            #
+            # flow_up[flow_up < sparse_threshold] = 0
+            # sparse_matrix = scipy.sparse.csc_matrix(flow_up)
+            # scipy.sparse.save_npz(os.path.join(flow_uv_sparse_path, image_file_name_1_flow_uv), sparse_matrix)
 
 
 if __name__ == '__main__':
@@ -71,5 +100,4 @@ if __name__ == '__main__':
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
     args = parser.parse_args()
-
     demo(args)
